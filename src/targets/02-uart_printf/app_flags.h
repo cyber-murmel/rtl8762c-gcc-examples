25faef7b4f0174f1e4cd43c02480b246d613b804
cyber-murmel <marble@chaoscomputer.club>
2022-06-07 02:20:57 +0200

    explain target contents

--- src/targets/02-uart_printf/app_flags.h
+++ src/targets/02-uart_printf/app_flags.h
@@ -1,34 +1,23 @@
 #ifndef _APP_FLAGS_H_
 #define _APP_FLAGS_H_
 
-/** @defgroup  PERIPH_Config Peripheral App Configuration
- * @brief This file is used to config app functions.
- * @{
- */
-/*============================================================================*
- *                              Constants
- *============================================================================*/
-
-/** @brief  Config APP LE link number */
+/** Config APP LE link number */
 #define APP_MAX_LINKS 1
-/** @brief  Config APP LE bond number */
+/** Config APP LE bond number */
 #define APP_MAX_BONDS 4
-/** @brief  Config DLPS: 0-Disable DLPS, 1-Enable DLPS */
+/** Config DLPS: 0-Disable DLPS, 1-Enable DLPS */
 #define F_BT_DLPS_EN 0
-/** @brief  Config ANCS Client: 0-Not built in, 1-Open ANCS client function */
+/** Config ANCS Client: 0-Not built in, 1-Open ANCS client function */
 #define F_BT_ANCS_CLIENT_SUPPORT 0
 #define F_BT_ANCS_APP_FILTER (F_BT_ANCS_CLIENT_SUPPORT & 1)
 #define F_BT_ANCS_GET_APP_ATTR (F_BT_ANCS_CLIENT_SUPPORT & 0)
-/** @brief  Config ANCS Client debug log: 0-close, 1-open  */
+/** Config ANCS Client debug log: 0-close, 1-open  */
 #define F_BT_ANCS_CLIENT_DEBUG (F_BT_ANCS_CLIENT_SUPPORT & 0)
 
-/**
- * @defgroup App_Flags_Realtek_Libs Undocumented Realtek flags.
- * @brief Flags for activating some undocumented features within the Realtek
- * libs. All flags are set to 0 because they were't defined and defaultet to 0.
- * Now they must be defined because of activating -Wundefined within the
- * makefile to prevent configuration failures without any warnings or errors.
- * @{
+/** Flags for activating some undocumented features within the Realtek
+ *  libs. All flags are set to 0 because they were't defined and defaultet to 0.
+ *  Now they must be defined because of activating -Wundefined within the
+ *  makefile to prevent configuration failures without any warnings or errors.
  */
 /** Enable write hardfault record to flash. */
 #define ENABLE_WRITE_HARDFAULT_RECORD_TO_FLASH 0
@@ -56,7 +45,4 @@
 /** Enable multibonding configuration.*/
 // #define BT_STACK_CONFIG_ENABLE
 
-/** @} */ /* App_Flags_Realtek_Libs */
-
-/** @} */ /* End of group PERIPH_Config */
 #endif
---
+++ src/targets/README.md
@@ -0,0 +1,5 @@
+# Targets
+Each target directory contains
+- application specific source code
+- `mksrc.mk` declaring all source files
+- `app_flags` defining app specific values used by the SDK
