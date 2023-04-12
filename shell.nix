3a5ef3808c35600bbb0c4daa43a04185092860ae
marble <git@computer-in.love>
2023-04-12 20:41:34 +0200

    update tools

--- shell.nix
+++ shell.nix
@@ -1,24 +1,24 @@
 {
-  pkgs ?
-  # nixpkgs 21.11, deterministic. Last updated: 2022-01-01.
-  import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/8a053bc.tar.gz")) {}
+  # nixpkgs 22.11, deterministic. Last updated: 2023-04-12.
+  pkgs ? import (fetchTarball ("https://github.com/NixOS/nixpkgs/archive/115a96e2ac1e92937cd47c30e073e16dcaaf6247.tar.gz")) { }
 }:
 
 with pkgs;
 let
   my-python-packages = python-packages: with python-packages; [
-    pyserial crccheck coloredlogs
+    pyserial
+    crccheck
+    coloredlogs
+    ipython
   ];
   python-with-my-packages = python3.withPackages my-python-packages;
-
-  openocd_0_10_0 = (import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz")) {}).openocd;
 in
 mkShell {
   buildInputs = [
     gnumake
-    gcc-arm-embedded-9
+    gcc-arm-embedded-10
     clang-tools
     python-with-my-packages
-    openocd_0_10_0
+    openocd
   ];
 }
