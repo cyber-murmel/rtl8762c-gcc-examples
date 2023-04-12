11104150c4c21a7c95264629f67665df85d3f6ea
marble <git@computer-in.love>
2023-04-12 20:42:02 +0200

    suppress compiler warning

--- src/targets/00-blink/main.c
+++ src/targets/00-blink/main.c
@@ -3,6 +3,9 @@
 #include "board_config.h"
 #include "pin/pin.h"
 
+// prevent `warning: function with qualified void return type called`
+#define delay_ms(T) (((void (*)(uint32_t)) platform_delay_ms)(T))
+
 int main(void)
 {
     pin_t led_pin = {
@@ -16,6 +19,6 @@ int main(void)
 
     while (1) {
         pin_set(&led_pin, !pin_get(&led_pin));
-        platform_delay_ms(1000);
+        delay_ms(1000);
     }
 }
