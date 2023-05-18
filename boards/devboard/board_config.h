70819c5fa900d33a9100cd489b97abdd925b434c
marble <git@computer-in.love>
2023-05-19 01:50:31 +0200

    set full mcu name

--- boards/devboard/board_config.h
+++ boards/devboard/board_config.h
@@ -2,7 +2,7 @@
 #define _BOARDS_DEVBOARD_BOARD_CONFIG_H
 
 #define BOARD_NAME "devboard"
-#define MCU_NAME "RTL8762C"
+#define MCU_NAME "RTL8762CKF"
 
 #define BOARD_TX_PAD (P3_0)
 #define BOARD_RX_PAD (P3_1)
