089d9d1251a21cf38d6d6e8b4017dabda4b7071e
marble <git@computer-in.love>
2023-05-19 01:50:19 +0200

    add include guards

--- boards/devboard/board_config.h
+++ boards/devboard/board_config.h
@@ -1,3 +1,6 @@
+#ifndef _BOARDS_DEVBOARD_BOARD_CONFIG_H
+#define _BOARDS_DEVBOARD_BOARD_CONFIG_H
+
 #define BOARD_NAME "devboard"
 #define MCU_NAME "RTL8762C"
 
@@ -5,3 +8,5 @@
 #define BOARD_RX_PAD (P3_1)
 
 #define LED_PAD (P0_6)
+
+#endif // _BOARDS_DEVBOARD_BOARD_CONFIG_H
